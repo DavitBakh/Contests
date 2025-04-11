f2434b801085a3f26004286d08ea433524bb26a9
Davit Bakhshyan <a1l2o3@bk.ru>
2025-04-12 00:04:01 +0400

    Some functions added to utils

--- Contests/Utils.cpp
+++ Contests/Utils.cpp
@@ -3,26 +3,87 @@
 
 using namespace std;
 
-#define all(a) a.begin(), a.end()
-
-void Foo()
+//finds the cycle in the graph
+bool dfs(int v, int prev, vector<list<int>>& g, vector<int>& used, vector<int>& path)
 {
-	ios_base::sync_with_stdio(0);
-	cout.tie(0);
-	cin.tie(0);
+	used[v] = 1;
+	path.push_back(v);
 
-	int n;
-	std::cin >> n;
-
-	vector<int> vec(n);
-
-	for (int i = 0; i < n; i++)
+	for (auto& to : g[v])
 	{
-		std::cin >> vec[i];
+		if (!used[to])
+			if (dfs(to, v, g, used, path))
+				return true;
+
+		if (used[to] == 1 && to != prev)
+		{
+			auto it = find(all(path), to);
+			std::cout << distance(it, path.end()) + 1 << std::endl;
+			while (it != path.end())
+			{
+				std::cout << *it + 1 << ' ';
+				it++;
+			}
+			std::cout << to + 1 << std::endl;
+
+			return true;
+		}
+
 	}
 
+	used[v] = 2;
+	path.pop_back();
 
-	std::cout << *min_element(all(vec)) << std::endl;
+	return false;
+}
 
+void dfs(int v, vector<list<int>>& g, vector<int>& used)
+{
+	used[v] = 1;
+	for (auto& to : g[v])
+	{
+		if (!used[to])
+			dfs(to, g, used);
+	}
+}
 
+void bfs(int source, vector<list<int>>& graph, vector<char>& used)
+{
+	int n = graph.size();
+
+	queue<int> q;
+	q.push(source);
+	used[source] = 1;
+
+	while (!q.empty())
+	{
+		int curr = q.front();
+		q.pop();
+
+		for (auto& e : graph[curr])
+		{
+			if (used[e] == 0)
+			{
+				q.push(e);
+				used[e] = 1;
+			}
+		}
+	}
+}
+
+int gcd(int a, int b, int& x, int& y)
+{
+	if (b == 0)
+	{
+		x = 1;
+		y = 0;
+		return a;
+	}
+
+	int tx, ty;
+	int c = gcd(b, a % b, tx, ty);
+	x = ty;
+	y = tx - (a / b) * ty;
+
+	return c;
 }
