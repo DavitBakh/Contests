50cb0cbe93377d7ea0eec2ded7a1e5e0476421ba
Davit Bakhshyan <a1l2o3@bk.ru>
2025-04-19 14:17:33 +0400

    Added Z funcion

--- Contests/Utils.hpp
+++ Contests/Utils.hpp
@@ -8,76 +8,76 @@
 
 void CopyToClipboard(const std::string& text)
 {
-    // Open the clipboard
-    if (!OpenClipboard(nullptr))
-    {
-        std::cerr << "Failed to open clipboard!" << std::endl;
-        return;
-    }
+	// Open the clipboard
+	if (!OpenClipboard(nullptr))
+	{
+		std::cerr << "Failed to open clipboard!" << std::endl;
+		return;
+	}
 
-    // Empty the clipboard
-    EmptyClipboard();
+	// Empty the clipboard
+	EmptyClipboard();
 
-    // Allocate a global memory object for the text
-    HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, text.size() + 1);
-    if (!hMem)
-    {
-        std::cerr << "Failed to allocate global memory!" << std::endl;
-        CloseClipboard();
-        return;
-    }
+	// Allocate a global memory object for the text
+	HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, text.size() + 1);
+	if (!hMem)
+	{
+		std::cerr << "Failed to allocate global memory!" << std::endl;
+		CloseClipboard();
+		return;
+	}
 
-    // Copy the text to the memory object
-    memcpy(GlobalLock(hMem), text.c_str(), text.size() + 1);
-    GlobalUnlock(hMem);
+	// Copy the text to the memory object
+	memcpy(GlobalLock(hMem), text.c_str(), text.size() + 1);
+	GlobalUnlock(hMem);
 
-    // Set the clipboard data
-    if (!SetClipboardData(CF_TEXT, hMem))
-    {
-        std::cerr << "Failed to set clipboard data!" << std::endl;
-        GlobalFree(hMem);
-        CloseClipboard();
-        return;
-    }
+	// Set the clipboard data
+	if (!SetClipboardData(CF_TEXT, hMem))
+	{
+		std::cerr << "Failed to set clipboard data!" << std::endl;
+		GlobalFree(hMem);
+		CloseClipboard();
+		return;
+	}
 
-    // Close the clipboard
-    CloseClipboard();
-    std::cout << "Text copied to clipboard successfully!" << std::endl;
+	// Close the clipboard
+	CloseClipboard();
+	std::cout << "Text copied to clipboard successfully!" << std::endl;
 }
-std::string GetFromClipboard() 
+std::string GetFromClipboard()
 {
-    std::string clipboardText;
+	std::string clipboardText;
 
-    // Open the clipboard
-    if (!OpenClipboard(nullptr)) 
-    {
-        std::cerr << "Failed to open clipboard!" << std::endl;
-        return clipboardText;
-    }
+	// Open the clipboard
+	if (!OpenClipboard(nullptr))
+	{
+		std::cerr << "Failed to open clipboard!" << std::endl;
+		return clipboardText;
+	}
 
-    // Get clipboard data in CF_TEXT format
-    HANDLE hData = GetClipboardData(CF_TEXT);
-    if (!hData) 
-    {
-        std::cerr << "Failed to get clipboard data!" << std::endl;
-        CloseClipboard();
-        return clipboardText;
-    }
+	// Get clipboard data in CF_TEXT format
+	HANDLE hData = GetClipboardData(CF_TEXT);
+	if (!hData)
+	{
+		std::cerr << "Failed to get clipboard data!" << std::endl;
+		CloseClipboard();
+		return clipboardText;
+	}
 
-    // Lock the handle to get the actual text pointer
-    char* pszText = static_cast<char*>(GlobalLock(hData));
-    if (pszText) 
-    {
-        clipboardText = pszText;  // Copy the text from the clipboard
-        GlobalUnlock(hData);
-    }
-    else
-        std::cerr << "Failed to lock global memory!" << std::endl;
+	// Lock the handle to get the actual text pointer
+	char* pszText = static_cast<char*>(GlobalLock(hData));
+	if (pszText)
+	{
+		clipboardText = pszText;  // Copy the text from the clipboard
+		GlobalUnlock(hData);
+	}
+	else
+		std::cerr << "Failed to lock global memory!" << std::endl;
 
-    // Close the clipboard
-    CloseClipboard();
+	// Close the clipboard
+	CloseClipboard();
 
-    return clipboardText;
+	return clipboardText;
 }
 
 using namespace std;
@@ -165,6 +165,27 @@ int gcd(int a, int b, int& x, int& y)
 	y = tx - (a / b) * ty;
 
 	return c;
+}
+
+void GetZF(const string& str, vector<int>& z)
+{
+	int n = str.size();
+
+	int l = 0, r = 0;
+	for (int i = 1; i < n; ++i)
+	{
+		if (r >= i)
+			z[i] = min(z[i - l], r - i + 1);
+
+		while (z[i] + i < n && str[z[i]] == str[z[i] + i])
+			++z[i];
+
+		if (i + z[i] - 1 > r)
+		{
+			l = i;
+			r = i + z[i] - 1;
+		}
+	}
 }
 
 
